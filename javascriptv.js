00b8e652d983179b0dc33f45f40953e7d97994ae
Vinay08082 <149697387+Vinay08082@users.noreply.github.com>
2024-03-25 23:08:27 +0530

    added indexv.html and stylev.css and javascript.js

---
+++ javascriptv.js
@@ -0,0 +1,6 @@
+console.log("hello welcome to personal portfolio");
+let btn=document.querySelector(".btn");
+btn.addEventListener("click",()=>{
+console.log("button clicked");
+btn.style.backgroundColor = "#555";
+})
